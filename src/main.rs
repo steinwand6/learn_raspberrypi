7c41068c7fc107064519b3ad66450bc89d10066c
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-09-14 02:00:45 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
+
+use rppal::gpio::Gpio;
+use rppal::system::DeviceInfo;
+
+const GPIO_LED: u8 = 0;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
+
+    let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
+
+    // Blink the LED by setting the pin's logic level high for 500 ms.
+    pin.set_high();
+    thread::sleep(Duration::from_millis(500));
+    pin.set_low();
+
+    Ok(())
+}
