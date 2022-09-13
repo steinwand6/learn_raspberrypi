9a35154cc3c5a1950fcfea79c181ab7a65f70313
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-09-14 02:22:15 +0900

    blink LED

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
 
     // Blink the LED by setting the pin's logic level high for 500 ms.
-    pin.set_high();
-    thread::sleep(Duration::from_millis(500));
-    pin.set_low();
-
+    for _ in 0..10 {
+        pin.set_low();
+        println!("...LED on");
+        thread::sleep(Duration::from_millis(500));
+        pin.set_high();
+        println!("...LED off");
+        thread::sleep(Duration::from_millis(500));
+    }
     Ok(())
 }
