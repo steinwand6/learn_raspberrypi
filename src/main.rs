f7b85aba90bcf0582fe74a8674d26f5d82ae7269
tomoya ishigaki <steinwand.tomo@gmail.com>
2022-09-14 23:39:49 +0900

    add output.rs

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,6 @@
+pub mod output;
 use std::error::Error;
-use std::thread;
-use std::time::Duration;
-
-use rppal::gpio::Gpio;
-use rppal::system::DeviceInfo;
-
-const GPIO_LED: u8 = 0;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    blink_led()
-}
-
-fn blink_led() -> Result<(), Box<dyn Error>> {
-    println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
-
-    let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
-
-    // Blink the LED by setting the pin's logic level high for 500 ms.
-    for _ in 0..10 {
-        pin.set_low();
-        println!("...LED on");
-        thread::sleep(Duration::from_millis(500));
-        pin.set_high();
-        println!("...LED off");
-        thread::sleep(Duration::from_millis(500));
-    }
-    Ok(())
+    output::blink_led()
 }
